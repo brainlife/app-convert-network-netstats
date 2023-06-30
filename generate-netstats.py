7681ea3736cc3a5bf6d9f3a8385274a7ff4518fc
Brad Caron <bacaron@utexas.edu>
2023-06-30 14:03:19 -0500

    reason: add tmp tags and datatype tags to work with pybrainlife

--- generate-netstats.py
+++ generate-netstats.py
@@ -8,7 +8,7 @@ import jgf
 import igraph
 import json
 
-def build_network_df(path,subjectID,sessionID):
+def build_network_df(path,subjectID,sessionID,tags,datatype_tags):
 
     df = pd.DataFrame()
     igraphs = []
@@ -18,6 +18,8 @@ def build_network_df(path,subjectID,sessionID):
     
     df['subjectID'] = [ subjectID for f in range(len(igraphs)) ]
     df['sessionID'] = [ sessionID for f in range(len(igraphs)) ]
+    df['tags'] = [ tags for f in range(len(igraphs)) ]
+    df['datatype_tags'] = [ tags for f in range(len(igraphs)) ]
     df['igraph'] = igraphs
 
     return df
@@ -35,17 +37,31 @@ def main():
     else:
         sessionID = '1'
 
+    if 'tags' in config['_inputs'][0]['meta'].keys():
+        tags = config['_inputs'][0]['meta']['tags']
+    else:
+        tags = ['tmp']
+
+    if 'datatype_tags' in config['_inputs'][0]['meta'].keys():
+        datatype_tags = config['_inputs'][0]['meta']['datatype_tags']
+    else:
+        datatype_tags = ['tmp']
+
     if not os.path.isdir('net-stats'):
         os.mkdir('net-stats')
         os.mkdir('net-stats/net-stats')
 
-    networks_df = build_network_df(network,subjectID,sessionID)
+    networks_df = build_network_df(network,subjectID,sessionID,tags,datatype_tags)
 
     conmats, global_measures, local_measures = blmanip.parse_networks(networks_df)
 
+    conmats = conmats.drop(columns=['tags','datatype_tags'])
+
     global_measures = global_measures.reset_index(drop=True)
+    global_measures = global_measures.drop(columns=['tags','datatype_tags'])
 
     local_measures = local_measures.reset_index(drop=True)
+    local_measures = local_measures.drop(columns=['tags','datatype_tags'])
 
     conmats.to_csv('./net-stats/net-stats/conmats.csv')
     global_measures.to_csv('./net-stats/net-stats/global_measures.csv',index=False)
